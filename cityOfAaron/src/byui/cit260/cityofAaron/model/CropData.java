5bbc772164784dcdb939ab2d8b073c2130187207
Jack <jackcmcbride@yahoo.com>
2018-05-19 23:34:17 -0500

    Added CropData class
    Added CropData class and added CropData testing to Main()

---
+++ cityOfAaron/src/byui/cit260/cityofAaron/model/CropData.java
@@ -0,0 +1,167 @@
+/*
+ * The CropData() class file for the cityOfAaron project
+ * CIT-260
+ * Team members: Laura Mazariegos, Chuck Mikolyski, Jack McBride
+ */
+package byui.cit260.cityofAaron.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author jackmcbride
+ */
+public class CropData implements Serializable {
+
+    public CropData() {
+    }
+    
+    private int year;
+    private int population;
+    private int acresOwned;
+    private int cropYield;
+    private int wheatInStore;
+    private int numberWhoDied;
+    private int newPeople;
+    private int harvest;
+    private int harvestAfterOffering;
+    private int offering;
+    private int offeringBushels;
+    private int peopleFed;
+    private int acresPlanted;
+    private int numStarved;
+    private int eatenByRats;
+    private int wheatForPeople;
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public int getAcresOwned() {
+        return acresOwned;
+    }
+
+    public int getCropYield() {
+        return cropYield;
+    }
+
+    public int getWheatInStore() {
+        return wheatInStore;
+    }
+
+    public int getNumberWhoDied() {
+        return numberWhoDied;
+    }
+
+    public int getNewPeople() {
+        return newPeople;
+    }
+
+    public int getHarvest() {
+        return harvest;
+    }
+
+    public int getHarvestAfterOffering() {
+        return harvestAfterOffering;
+    }
+
+    public int getOffering() {
+        return offering;
+    }
+
+    public int getOfferingBushels() {
+        return offeringBushels;
+    }
+
+    public int getPeopleFed() {
+        return peopleFed;
+    }
+
+    public int getAcresPlanted() {
+        return acresPlanted;
+    }
+
+    public int getNumStarved() {
+        return numStarved;
+    }
+
+    public int getEatenByRats() {
+        return eatenByRats;
+    }
+
+    public int getWheatForPeople() {
+        return wheatForPeople;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public void setAcresOwned(int acresOwned) {
+        this.acresOwned = acresOwned;
+    }
+
+    public void setCropYield(int cropYield) {
+        this.cropYield = cropYield;
+    }
+
+    public void setWheatInStore(int wheatInStore) {
+        this.wheatInStore = wheatInStore;
+    }
+
+    public void setNumberWhoDied(int numberWhoDied) {
+        this.numberWhoDied = numberWhoDied;
+    }
+
+    public void setNewPeople(int newPeople) {
+        this.newPeople = newPeople;
+    }
+
+    public void setHarvest(int harvest) {
+        this.harvest = harvest;
+    }
+
+    public void setHarvestAfterOffering(int harvestAfterOffering) {
+        this.harvestAfterOffering = harvestAfterOffering;
+    }
+
+    public void setOffering(int offering) {
+        this.offering = offering;
+    }
+
+    public void setOfferingBushels(int offeringBushels) {
+        this.offeringBushels = offeringBushels;
+    }
+
+    public void setPeopleFed(int peopleFed) {
+        this.peopleFed = peopleFed;
+    }
+
+    public void setAcresPlanted(int acresPlanted) {
+        this.acresPlanted = acresPlanted;
+    }
+
+    public void setNumStarved(int numStarved) {
+        this.numStarved = numStarved;
+    }
+
+    public void setEatenByRats(int eatenByRats) {
+        this.eatenByRats = eatenByRats;
+    }
+
+    public void setWheatForPeople(int wheatForPeople) {
+        this.wheatForPeople = wheatForPeople;
+    }
+    
+    
+    
+    
+}
--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -9,6 +9,7 @@ import byui.cit260.cityofAaron.model.Player;
 import byui.cit260.cityofAaron.model.TeamMember;
 import byui.cit260.cityofAaron.model.Location;
 import byui.cit260.cityofAaron.model.ListItem;
+import byui.cit260.cityofAaron.model.CropData;
 /**
  *
  * @author Laura Mazariegos, Chuck Mikolyski, Jack McBride
@@ -55,6 +56,63 @@ public class CityOfAaron {
         int itemNumber = item.getItemNumber();
         System.out.println("Item: " + itemName + " = " + itemNumber);
         // End Laura's code
-                
+        
+        // Start Jack's code
+        CropData firstCrop = new CropData();
+        
+        firstCrop.setAcresOwned(1);
+        firstCrop.setAcresPlanted(2);
+        firstCrop.setCropYield(3);
+        firstCrop.setEatenByRats(4);
+        firstCrop.setHarvest(5);
+        firstCrop.setHarvestAfterOffering(6);
+        firstCrop.setNewPeople(7);
+        firstCrop.setNumStarved(8);
+        firstCrop.setNumberWhoDied(9);
+        firstCrop.setOffering(10);
+        firstCrop.setOfferingBushels(11);
+        firstCrop.setPeopleFed(12);
+        firstCrop.setPopulation(13);
+        firstCrop.setYear(14);
+        firstCrop.setWheatForPeople(15);
+        firstCrop.setWheatInStore(16);
+        
+        int acresOwned = firstCrop.getAcresOwned();
+        int acresPlanted = firstCrop.getAcresPlanted();
+        int cropYield = firstCrop.getCropYield();
+        int eatenByRats = firstCrop.getEatenByRats();
+        int harvest = firstCrop.getHarvest();
+        int harvestAfterOffering = firstCrop.getHarvestAfterOffering();
+        int newPeople = firstCrop.getNewPeople();
+        int numStarved = firstCrop.getNumStarved();
+        int numDied = firstCrop.getNumberWhoDied();
+        int offering = firstCrop.getOffering();
+        int offeringBushels = firstCrop.getOfferingBushels();
+        int peopleFed = firstCrop.getPeopleFed();
+        int population = firstCrop.getPopulation();
+        int year = firstCrop.getYear();
+        int wheatForPeople = firstCrop.getWheatForPeople();
+        int wheatInStore = firstCrop.getWheatInStore();
+        
+        System.out.println("\nJack's tests:");
+        System.out.println("Should be 1: " + acresOwned);
+        System.out.println("Should be 2: " + acresPlanted);
+        System.out.println("Should be 3: " + cropYield);
+        System.out.println("Should be 4: " + eatenByRats);
+        System.out.println("Should be 5: " + harvest);
+        System.out.println("Should be 6: " + harvestAfterOffering);
+        System.out.println("Should be 7: " + newPeople);
+        System.out.println("Should be 8: " + numStarved);
+        System.out.println("Should be 9: " + numDied);
+        System.out.println("Should be 10: " + offering);
+        System.out.println("Should be 11: " + offeringBushels);
+        System.out.println("Should be 12: " + peopleFed);
+        System.out.println("Should be 13: " + population);
+        System.out.println("Should be 14: " + year);
+        System.out.println("Should be 15: " + wheatForPeople);
+        System.out.println("Should be 16: " + wheatInStore);  
+        System.out.println("End of Jack's tests.\n");
+        // End Jack's code
+
     }
 }
