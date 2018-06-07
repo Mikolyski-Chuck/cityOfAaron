63c0a441d7b25673b4e292a02004f9c54a9c9d27
Jack <jackcmcbride@yahoo.com>
2018-06-06 22:58:58 -0500

    Add Main Menu View

---
+++ cityOfAaron/src/byui/cit260/cityofAaron/view/MainMenuView.java
@@ -0,0 +1,177 @@
+/*
+ * The MainMenuView class file for the cityOfAaron project
+ * Part of the view layer
+ * Object of this class manages the main menu
+ * CIT-260
+ * Team members: Laura Mazariegos, Chuck Mikolyski, Jack McBride
+ * Date last modified: June 6, 2018
+ */
+package byui.cit260.cityofAaron.view;
+import byui.cit260.cityofAaron.model.Game;
+import byui.cit260.cityofAaron.model.Player;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jackmcbride
+ */
+public class MainMenuView {
+    
+    Scanner keyboard = new Scanner(System.in);
+    private String theMenu;
+    private int max;
+    
+    // The displayMenuView method
+    // Purpose: displays the menu, gets the user's input, and
+    //          does the selected action
+    // Parameters: none
+    // Returns: none
+    // =======================================================
+    public void displayMenuView()
+    {
+        int menuOption;
+        do
+        {
+           // Display the menu
+           System.out.println(theMenu);
+           // Prompt the user and get the user's input
+           menuOption = getMenuOption();
+           // Perform the desired action
+           doAction(menuOption);
+        // Determine and display the next 
+        } while (menuOption != max);
+        
+    }
+    
+    // The MainMenuView constructor
+    // Purpose: Initialize the menu data
+    // Parameters: none
+    // Returns: none
+    // =======================================================
+    public MainMenuView()
+    {
+        theMenu = "\n" + 
+                "**************************\n" +
+                "* CITY OF AARON: MAIN GAME MENU *\n" +
+                "**************************\n" +
+                " 1 - Start new game\n" +
+                " 2 - Get and start a saved game\n" +
+                " 3 - Get help on playing the game\n" +
+                " 4 - Save game\n" +
+                " 5 - Quit\n";
+        
+        max = 5;
+    }
+    
+    // The getMenuOption method
+    // Purpose: gets the user's input
+    // Parameters: none
+    // Returns: integer - the option selected
+    // =======================================================
+    public int getMenuOption()
+    {
+        // declare a variable to hold user's input
+        int userInput;
+        // begin loop
+        do
+        {
+        // get user input from the keyboard
+            userInput = keyboard.nextInt();
+        // if it is not a valid value, output an error message
+            if(userInput < 1 || userInput > max)
+            {
+                System.out.println("\nOption must be between 1 and " + max);
+            }
+        // loop back to the top if input was not valid
+        } while(userInput < 1 || userInput > max);
+        // return the value input by the user
+        return userInput;
+    }
+    
+    // The doAction method
+    // Purpose: performs the selected action
+    // Parameters: none
+    // Returns: none
+    // =======================================================
+    public void doAction(int option)
+    {
+        switch(option)
+        {
+            case 1: // if the option is 1, call startNewGame()
+                startNewGame();
+                break;
+            case 2: // if the option is 2, call startExisting()
+                startSavedGame();
+                break;
+            case 3: // if the option is 3, call displayHelpMenu()
+                displayHelpMenuView();
+                break;
+            case 4: // if the option is 4, call displaySaveGame()
+                displaySaveGameView();
+                break;
+            case 5: // if the option is 5, display a goodbye message
+                System.out.println("Thanks for playing... goodbye.");
+        }
+    }
+    
+    // The startNewGame method
+    // Purpose: creates game object and starts the game
+    // Parameters: none
+    // Returns: none
+    // =======================================================
+    public void startNewGame()
+    {
+        // create a new game object
+        Game theGame = new Game();
+        // save a reference to it in the GameProject class
+        cityofaaron.CityOfAaron.setTheGame(theGame);
+        // display the banner page.
+        System.out.println("\nWelcome to the city of Aaron.");
+        // create a new player object
+        Player thePlayer = new Player();
+        // prompt for and get the user's name
+        String name;
+        System.out.println("\nPlease type in your first name: ");
+        name = keyboard.next();
+        // save the user's name in the Player object
+        thePlayer.setName(name);
+        // save a reference to the Player object in the Game object
+        theGame.setThePlayer(thePlayer);
+        // display a welcome message
+        System.out.println("\nWelcome " + name + " have fun.");
+        // display the Game menu    
+    }
+    
+    // The startSavedGame method
+    // Purpose: loads the game object and starts the game
+    // Parameters: none
+    // Returns: none
+    // =======================================================
+    public void startSavedGame()
+    {
+       System.out.println("\nStart saved game menu option selected.");
+
+    }
+    
+    // The displayHelpMenuView method
+    // Purpose: displays the help menu view
+    // Parameters: none
+    // Returns: none
+    // =======================================================
+    public void displayHelpMenuView()
+    {
+        System.out.println("\nDisplay help menu option selected.");
+    }
+    
+    // The displaySaveGameView method
+    // Purpose: displays the save game view
+    // Parameters: none
+    // Returns: none
+    // =======================================================
+    public void displaySaveGameView()
+    {
+        System.out.println("\nDisplay save game option selected.");
+    }
+}
+
+
--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -5,99 +5,113 @@
  */
 package cityofaaron;
 
-import byui.cit260.cityofAaron.model.Player;
-import byui.cit260.cityofAaron.model.TeamMember;
-import byui.cit260.cityofAaron.model.Location;
-import byui.cit260.cityofAaron.model.ListItem;
-import byui.cit260.cityofAaron.model.CropData;
+import byui.cit260.cityofAaron.view.*;
+
+import byui.cit260.cityofAaron.model.Game;
 /**
  *
  * @author Laura Mazariegos, Chuck Mikolyski, Jack McBride
  */
 public class CityOfAaron {
+    
+    private static Game theGame = null;
 
+    public static Game getTheGame() {
+        return theGame;
+    }
+
+    public static void setTheGame(Game theGame) {
+        CityOfAaron.theGame = theGame;
+    }
+
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Player playerOne = new Player();
         
-        playerOne.setName("Ted Tintstone");
-            
-        String name = playerOne.getName();
+        MainMenuView mmv = new MainMenuView();
+        mmv.displayMenuView();
+        
+        
+//        Player playerOne = new Player();
 //        
-        String member1Name = TeamMember.Member1.getName();
-        String member2Name = TeamMember.Member2.getName();
-        String member3Name = TeamMember.Member3.getName();
-        
-        String member1Desc = TeamMember.Member1.getDescription();
-        String member2Desc = TeamMember.Member2.getDescription();
-        String member3Desc = TeamMember.Member3.getDescription();
-        
-         
-        System.out.println(name);
-        System.out.println(member1Name + ", " + member1Desc + ", " + member2Name + ", "  + member2Desc + ", and " + member3Name + ", " + member3Desc + ", are the coolest.");
-        
-        // Start Chuck Mikolyski
-        Location firstLocation = new Location();
-        firstLocation.setDescription("The Crop Fields");
-        firstLocation.setSymbol("|||");
-        String description = firstLocation.getDescription();
-        String symbol = firstLocation.getSymbol();
-        System.out.println("Location Description: " + description +  "\nLocation"
-        + " Symbol: " + symbol);
-        // End Chuck Mikolyski
-        
-        // Laura's code
-        ListItem item = new ListItem();
-        item.setItemName("bushels");
-        item.setItemNumber(0);
-        String itemName = item.getItemName();
-        int itemNumber = item.getItemNumber();
-        System.out.println("Item: " + itemName + " = " + itemNumber);
-        // End Laura's code
-        
-        // Start Jack's code
-        CropData firstCrop = new CropData();
-        
-        firstCrop.setAcresOwned(1);
-        firstCrop.setAcresPlanted(2);
-        firstCrop.setCropYield(3);
-        firstCrop.setEatenByRats(4);
-        firstCrop.setHarvest(5);
-        firstCrop.setHarvestAfterOffering(6);
-        firstCrop.setNewPeople(7);
-        firstCrop.setNumStarved(8);
-        firstCrop.setNumberWhoDied(9);
-        firstCrop.setOffering(10);
-        firstCrop.setOfferingBushels(11);
-        firstCrop.setPeopleFed(12);
-        firstCrop.setPopulation(13);
-        firstCrop.setYear(14);
-        firstCrop.setWheatForPeople(15);
-        firstCrop.setWheatInStore(16);
-        
-        int acresOwned = firstCrop.getAcresOwned();
-        int acresPlanted = firstCrop.getAcresPlanted();
-        int cropYield = firstCrop.getCropYield();
-        int eatenByRats = firstCrop.getEatenByRats();
-        int harvest = firstCrop.getHarvest();
-        int harvestAfterOffering = firstCrop.getHarvestAfterOffering();
-        int newPeople = firstCrop.getNewPeople();
-        int numStarved = firstCrop.getNumStarved();
-        int numDied = firstCrop.getNumberWhoDied();
-        int offering = firstCrop.getOffering();
-        int offeringBushels = firstCrop.getOfferingBushels();
-        int peopleFed = firstCrop.getPeopleFed();
-        int population = firstCrop.getPopulation();
-        int year = firstCrop.getYear();
-        int wheatForPeople = firstCrop.getWheatForPeople();
-        int wheatInStore = firstCrop.getWheatInStore();
-        
-        String testString = firstCrop.toString();
-        
-        System.out.println(testString);
-        // End Jack's code
+//        playerOne.setName("Ted Tintstone");
+//            
+//        String name = playerOne.getName();
+//       
+//        String member1Name = TeamMember.Member1.getName();
+//        String member2Name = TeamMember.Member2.getName();
+//        String member3Name = TeamMember.Member3.getName();
+//        
+//        String member1Desc = TeamMember.Member1.getDescription();
+//        String member2Desc = TeamMember.Member2.getDescription();
+//        String member3Desc = TeamMember.Member3.getDescription();
+//        
+//         
+//        System.out.println(name);
+//        System.out.println(member1Name + ", " + member1Desc + ", " + member2Name + ", "  + member2Desc + ", and " + member3Name + ", " + member3Desc + ", are the coolest.");
+//        
+//        // Start Chuck Mikolyski
+//        Location firstLocation = new Location();
+//        firstLocation.setDescription("The Crop Fields");
+//        firstLocation.setSymbol("|||");
+//        String description = firstLocation.getDescription();
+//        String symbol = firstLocation.getSymbol();
+//        System.out.println("Location Description: " + description +  "\nLocation"
+//        + " Symbol: " + symbol);
+//        // End Chuck Mikolyski
+//        
+//        // Laura's code
+//        ListItem item = new ListItem();
+//        item.setItemName("bushels");
+//        item.setItemNumber(0);
+//        String itemName = item.getItemName();
+//        int itemNumber = item.getItemNumber();
+//        System.out.println("Item: " + itemName + " = " + itemNumber);
+//        // End Laura's code
+//        
+//        // Start Jack's code
+//        CropData firstCrop = new CropData();
+//        
+//        firstCrop.setAcresOwned(1);
+//        firstCrop.setAcresPlanted(2);
+//        firstCrop.setCropYield(3);
+//        firstCrop.setEatenByRats(4);
+//        firstCrop.setHarvest(5);
+//        firstCrop.setHarvestAfterOffering(6);
+//        firstCrop.setNewPeople(7);
+//        firstCrop.setNumStarved(8);
+//        firstCrop.setNumberWhoDied(9);
+//        firstCrop.setOffering(10);
+//        firstCrop.setOfferingBushels(11);
+//        firstCrop.setPeopleFed(12);
+//        firstCrop.setPopulation(13);
+//        firstCrop.setYear(14);
+//        firstCrop.setWheatForPeople(15);
+//        firstCrop.setWheatInStore(16);
+//        
+//        int acresOwned = firstCrop.getAcresOwned();
+//        int acresPlanted = firstCrop.getAcresPlanted();
+//        int cropYield = firstCrop.getCropYield();
+//        int eatenByRats = firstCrop.getEatenByRats();
+//        int harvest = firstCrop.getHarvest();
+//        int harvestAfterOffering = firstCrop.getHarvestAfterOffering();
+//        int newPeople = firstCrop.getNewPeople();
+//        int numStarved = firstCrop.getNumStarved();
+//        int numDied = firstCrop.getNumberWhoDied();
+//        int offering = firstCrop.getOffering();
+//        int offeringBushels = firstCrop.getOfferingBushels();
+//        int peopleFed = firstCrop.getPeopleFed();
+//        int population = firstCrop.getPopulation();
+//        int year = firstCrop.getYear();
+//        int wheatForPeople = firstCrop.getWheatForPeople();
+//        int wheatInStore = firstCrop.getWheatInStore();
+//        
+//        String testString = firstCrop.toString();
+//        
+//        System.out.println(testString);
+//        // End Jack's code
 
     }
 }
