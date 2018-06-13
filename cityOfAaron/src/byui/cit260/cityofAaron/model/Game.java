a1bc7892d5bec0c87b38cf4bc35f955f080a5f83
CMikolyski <>
2018-06-12 19:41:01 -0500

    06/012/2018 Add cropData object reference to Game Class in the model layer. Getters and Setters included as well.

--- cityOfAaron/src/byui/cit260/cityofAaron/model/Game.java
+++ cityOfAaron/src/byui/cit260/cityofAaron/model/Game.java
@@ -13,7 +13,27 @@ import java.io.Serializable;
  */
 public class Game implements Serializable {
     private  Player thePlayer;
+    private CropData cropData = null;
 
+    
+    // The getCropData() method
+    // Purpose: Get a reference to the crop object
+    // Parameters: None
+    // Returns: A reference to a crop object
+    public CropData getCropData() 
+    {
+        return cropData;
+    }
+    
+    // The setCropData() Method
+    // Purpose: Store a reference to a crop object
+    // Parameters: A Reference to a crop object
+    // Returns: None
+    public void setCropData(CropData _cropRef) 
+    {
+        cropData = _cropRef;
+    }
+    
     public Player getThePlayer() {
         return thePlayer;
     }
