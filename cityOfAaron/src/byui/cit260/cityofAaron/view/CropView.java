3e55efaf4f27a4b93622c78a74bb6686fdec4b7b
CMikolyski <>
2018-06-13 10:51:36 -0500

    06/13/2018 Add feddPeopleView method to CropView Class.

--- cityOfAaron/src/byui/cit260/cityofAaron/view/CropView.java
+++ cityOfAaron/src/byui/cit260/cityofAaron/view/CropView.java
@@ -21,8 +21,6 @@ import cityofaaron.CityOfAaron;
 public class CropView 
 {
     
-    
-
     // Create a Scanner Object
     private static Scanner keyboard = new Scanner(System.in);
     
@@ -30,7 +28,27 @@ public class CropView
     private static Game theGame = CityOfAaron.getTheGame();
     private static CropData cropData = theGame.getCropData();
     
-
+    // Start Chuck Mikolyski
+    // The feedPeopleView method
+    // Purpose: Interface with the user input to set food aside to feed the
+    // people
+    // Parameters: None
+    // Returns: None
+    public static void feedPeopleView()
+    {
+        // Prompt the user to enter how much wheat they would like to set aside
+        // to feed the people
+        System.out.println("How much wheat would you like to set aside to " + 
+                           "feed the people?");
+        
+        // Get the user input and save it
+        int wheatSetAside = keyboard.nextInt();
+        
+        // Call the feedPeople() method to set aside food to feed the people.
+        CropControl.feedPeople(wheatSetAside, cropData);
+    }   
+    // End Chuck Mikolyski
+    
     // The buyLandView method
     // Purpose: Interface with the user input for buying land
     // Parameters: None
@@ -42,7 +60,7 @@ public class CropView
         
         // Prompt the user to enter the number of acres to buy
         System.out.format("Land is selling for %d bushels per acre %n", price);
-        System.out.print("\nHow many acres ot land do you wish to buy?");
+        System.out.println("\nHow many acres of land do you wish to buy?");
         
         // Get the user's input and save it
         int toBuy = keyboard.nextInt();
@@ -60,7 +78,7 @@ public class CropView
      
         // Call the buyLandView() Method
         buyLandView();
-        
+        feedPeopleView();
         // Add calls to the other crop view methods as they are written
     }
 
