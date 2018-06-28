d3be7dfe6ea59a3580ec25c6d2bfe82410566cc0
Jack <jackcmcbride@yahoo.com>
2018-06-27 23:35:49 -0400

    Individual Assignment - 9
    Added inheritance to Game Menu

--- cityOfAaron/src/byui/cit260/cityofAaron/view/GameMenuView.java
+++ cityOfAaron/src/byui/cit260/cityofAaron/view/GameMenuView.java
@@ -14,43 +14,18 @@ import java.util.Scanner;
  *
  * @author Jack McBride
  */
-public class GameMenuView {
-    
-private String gameMenu;
-private int max;
-Scanner keyboard = new Scanner(System.in);
-private Game theGame;
-
-// The displayMenuView method
-    // Purpose: displays the Game menu, gets the user's input, and
-    // does the selected action
-    // Parameters: none
-    // Returns: none
-    // =======================================================
-    public void displayMenuView()
-    {
-        int menuOption;
-        do
-        {
-           // Display the menu
-           System.out.println(gameMenu);
-           // Prompt the user and get the user's input
-           menuOption = getMenuOption();
-           // Perform the desired action
-           doAction(menuOption);
-        // Determine and display the next 
-        } while (menuOption != max);
-        
-    }
+public class GameMenuView extends MenuView {
     
     // The GameMenuView constructor
     // Purpose: Initialize the game menu data
     // Parameters: none
     // Returns: none
     // =======================================================
+    
+       
     public GameMenuView()
     {
-        gameMenu = "\n" + 
+        super("\n" + 
                 "**************************\n" +
                 "* GAME MENU *\n" +
                 "**************************\n" +
@@ -58,34 +33,7 @@ private Game theGame;
                 " 2 - View/Print a list\n" +
                 " 3 - Move to a new location\n" +
                 " 4 - Manage the Crops\n" +
-                " 5 - Return to the Main Menu\n";
-               
-        max = 5;
-    }
-    
-    // The getMenuOption method
-    // Purpose: gets the user's input
-    // Parameters: none
-    // Returns: integer - the option selected
-    // =======================================================
-    public int getMenuOption()
-    {
-        // declare a variable to hold user's input
-        int userInput;
-        // begin loop
-        do
-        {
-        // get user input from the keyboard
-            userInput = keyboard.nextInt();
-        // if it is not a valid value, output an error message
-            if(userInput < 1 || userInput > max)
-            {
-                System.out.println("\nOption must be between 1 and " + max);
-            }
-        // loop back to the top if input was not valid
-        } while(userInput < 1 || userInput > max);
-        // return the value input by the user
-        return userInput;
+                " 5 - Return to the Main Menu\n", 5);
     }
     
     // The doAction method
@@ -93,7 +41,7 @@ private Game theGame;
     // Parameters: none
     // Returns: none
     // =======================================================
-    public void doAction(int option)
+    @Override public void doAction(int option)
     {
         switch(option)
         {
