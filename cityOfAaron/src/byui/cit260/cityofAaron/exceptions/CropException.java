cac708748fc8a9e8411004562a3bc5a0f1bc923d
CMikolyski <>
2018-07-05 19:07:32 -0500

    07/05/2018 Added CropException Class.

---
+++ cityOfAaron/src/byui/cit260/cityofAaron/exceptions/CropException.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.cityofAaron.exceptions;
+
+/**
+ *
+ * @author Christine
+ */
+public class CropException extends Exception
+{
+    //Default Constructor
+    //Purpose: Initializes data members to default values
+    //Parameters: non
+    //Returns: none
+    public CropException(){}
+
+    //Parameterized constructor
+    //Purpose: Initializes data members to value passed as a parameter
+    //Parameters: A string containing a error message
+    //Returns: none
+    public CropException(String string)
+    {
+        super(string);
+    }
+}
